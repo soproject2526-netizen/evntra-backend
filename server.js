d9cdeea2aa0aed55db439bf411753c2c164d2985
Omkar <soproject2526@gmail.com>
2026-03-01 16:45:19 +0530

    Clean backend final

---
+++ server.js
@@ -0,0 +1,11 @@
+// src/server.js (updated)
+require('dotenv').config();
+const app = require('./app');
+const http = require('http');
+const server = http.createServer(app);
+const { initSocket } = require('./sockets/socketServer');
+
+const io = initSocket(server); 
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
