86ef90771b65ed6164c3b75314b07d1aa2669937
Omkar <soproject2526@gmail.com>
2026-03-20 18:18:25 +0530

    Backend code changes

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,9 +1,13 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
+console.log("Cloudinary Config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY ? "Loaded" : "Missing",
+  api_secret: process.env.CLOUDINARY_API_SECRET ? "Loaded" : "Missing",
+});
 module.exports = cloudinary;
