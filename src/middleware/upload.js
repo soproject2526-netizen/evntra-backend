676b209561f60d08fb9e37ba22fbe2e913764d2c
Omkar <soproject2526@gmail.com>
2026-04-08 11:44:45 +0530

    Commit the New backend Changes

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -12,10 +12,15 @@ if (!fs.existsSync(EVENTS_UPLOAD_DIR)) {
 const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image") || file.mimetype.startsWith("video")) {
+  console.log("MIME TYPE:", file.mimetype); //  DEBUG
+
+  if (
+    file.mimetype.startsWith('image/') ||
+    file.mimetype.startsWith('video/')
+  ) {
     cb(null, true);
   } else {
-    cb(new Error("Only images and videos are allowed"), false);
+    cb(new Error('Only images and videos are allowed'), false);
   }
 };
 
